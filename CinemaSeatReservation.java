46d59224c79b4b2faeaccff83d5f1766d3e7a8a4
okkessefa <oksekesfackn53@gmail.com>
2026-01-25 19:39:27 +0300

    CP1 : updating <Cinema seat reservation>

--- CinemaSeatReservation.java
+++ CinemaSeatReservation.java
@@ -1,76 +1,81 @@
 
 import java.util.Scanner;
 
-public class NewClass9 {
+public class CinemaSeatReservation {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int[][] seats = new int[5][6]; // 5 rows (A-E), 6 columns (1-6)
-        char[] label = {'A','B','C','D','E'};
-        boolean running = true;
+        int[][] hallHidden = new int[5][7];
+        char[][] hallVisual = new char[5][7];
+        char[] rowLabel = {'A','B','C','D','E'};
+        
+        for(int i = 0; i<hallVisual.length; i++){
+            for(int j= 0; j<hallVisual[0].length; j++){
+                hallVisual[i][j] = '0';
+            }
+         }
+        int userRow =-1;
+        int userColumn = -1;
         int price = 0;
-        do {
-            // 1. Print Layout using Nested Loops
-            System.out.println("  6  5  4  3  2  1"); // Column headers
-            for (int i = seats.length-1; i>=0; i--) {
-                System.out.print(label[i] + " "); // Row labels A, B, C...
-                for (int j = seats[0].length-1; j>=0; j--) {
-                    if(seats[i][j] == 0){
-                        System.out.print("O  ");
-                    }
-                    else{
-                        System.out.print("X  ");
-                    }
+        boolean isRunning = true;
+        do{
+            System.out.println();
+            System.out.print("\\ ");
+            for(int i =0; i<hallVisual[0].length; i++){
+                System.out.print(hallVisual[0].length-i + " ");
+            }
+            System.out.println();
+            for(int i = hallVisual.length-1; i>=0; i--){
+                
+                System.out.print(rowLabel[i]+" ");
+                for(int j= hallVisual[0].length-1; j>=0; j--){
+                    System.out.print(hallVisual[i][j] + " ");
                 }
                 System.out.println();
             }
-
-            // 2. Get User Input
-            System.out.print("\nEnter seat code : ");
-            String row  = scan.next().toUpperCase().trim();
-            System.out.print("Enter column number: ");
-            int column = scan.nextInt();
-            String labels = "ABCDE";
-            if (labels.contains(row) == false || row.length() != 1 || column < 1 || column > 6) {
-                System.out.println("Seat out of bounds!");
+            System.out.println();
+            System.out.print("Enter a row number (For quiting Enter -1): ");
+            userRow = scan.nextInt();
+            System.out.print("Enter a row number (For quiting Enter -1): ");
+            userColumn = scan.nextInt();
+            
+            if(userRow == -1 && userColumn == -1) {
+                isRunning = false;
+                break;
+            } 
+            if (userRow < 1 || userRow > 4 || userColumn < 1 || userColumn > 4) {
+                System.out.println("Please enter a valid seat number!");
                 continue;
             }
-            else{
-                if (seats[labels.indexOf(row)][column-1] == 1) {
-                    System.out.println("Seat already occupied!");
-                } else {
-                    seats[labels.indexOf(row)][column-1] = 1;
-                    if(labels.indexOf(row)<2){
-                        price+=120;
-                    }
-                    else{
-                        price+=70;
-                    }
-                    System.out.println("Seat booked successfully!");
-                }
+            if(hallHidden[userRow-1][userColumn-1] == 1){
+                System.out.println("This seat is booked, Choose another one ");
+                continue;
             }
-            do{
-                System.out.print("Want to book again: ");
-                char choice = scan.next().trim().toUpperCase().charAt(0);
-                if(choice == 'Y'){
-                    break;
+            switch(userRow){
+                case 1, 2 -> {
+                    price+=100;
+                    hallHidden[userRow-1][userColumn-1] = 1;
+                    hallVisual[userRow-1][userColumn-1] = 'X';
                 }
-                else if(choice == 'N'){
-                    System.out.println("Bye...");
-                    running = false;
-                    break;
+                case 3, 4 ->  { 
+                    price+=50;
+                    hallHidden[userRow-1][userColumn-1] = 1;
+                    hallVisual[userRow-1][userColumn-1] = 'X';
                 }
-                else{
-                    System.out.println("Enter valid input!");
-                }
-            }while(true);
-        }while (running);
-        System.out.println("Total price => "+ price);
-        System.out.println("Booked seats: ");
-        for(int i =0; i<seats.length; i++){
-            for(int j=0; j<seats[0].length; j++){
-                if(seats[i][j] == 1){
-                    System.out.print(label[i]);
-                    System.out.print(j + " / ");
+                default -> {
+                    System.out.println("Please enter a valid seat number!");
+                    continue;
+                }    
+            }
+        }while(isRunning);
+        
+        if(price>0){
+            System.out.println("Total Price is :" + price);
+            System.out.println("Booked Seat(s): ");
+            for(int i = 0; i<hallVisual.length; i++){
+                for(int j= 0; j<hallVisual[0].length; j++){
+                    if(hallHidden[i][j] == 1){
+                        System.out.print(rowLabel[i] + " " +j + " / ");
+                    }
                 }
             }
         }
