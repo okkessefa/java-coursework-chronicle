f620e3c3f05fe873a6d89a2df1322b79fc33258c
Okkes Sefa <oksekesfackn53@gmail.com>
2026-01-31 12:35:27 +0300

    Updating and dadding new information

--- EscapeFromDungeons.java
+++ EscapeFromDungeons.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.util.Random;
 public class EscapeFromDungeons {
     public static void main(String[] args) {
+        // Declaring global utilities
         Scanner scan = new Scanner(System.in);
         Random rand = new Random();
         
